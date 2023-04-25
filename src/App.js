d70bc8ddf58c00ca0c2c8590e8834658a39e7a13
hollowK <hiep.lp206@gmail.com>
2023-04-25 21:30:49 +0700

    Add UseState and UseEffect functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import api from './API/axiosConfig';
 import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+  
+  //Change state of movies var
+  //handle HTTP request
+
+  
+  const getMovies = async () => {
+
+    try
+    {
+
+      const response = await api.get("/api/v1/movies");
+  
+      console.log(response.data);
+
+      setMovies(response.data);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  },[])
+  
   return (
     <div className="App">
 
